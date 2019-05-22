d9006f4d01ca6e91f8e9b60cfb7cd549c3338c56
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-05-22 18:57:20 -0300

    fix: route

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/login', (req, res, next) => {
+app.use('/api/users/login', (req, res, next) => {
   res.cookie('auth', '123', {
     expires: new Date(Date.now() + 10000000)
   }).end()
